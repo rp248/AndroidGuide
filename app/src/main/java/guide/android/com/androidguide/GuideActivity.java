01286900adb2b7fcc2462dbf3e0cc2274e565ed8
rajeshpalivella <rajeshp248@gmail.com>
2016-06-13 14:52:18 +0530

    add activity callbacks, and new activities to manifest file

--- app/src/main/java/guide/android/com/androidguide/GuideActivity.java
+++ app/src/main/java/guide/android/com/androidguide/GuideActivity.java
@@ -1,17 +1,67 @@
 package guide.android.com.androidguide;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 public class GuideActivity extends AppCompatActivity {
 
+    final String TAG = "GuideActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
-        ViewGroup VG;
-        Button b;
+        Log.i(TAG,"onCreate() called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.i(TAG, "onSaveInstanceState() called");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState() called");
     }
 }
---
+++ app/src/main/java/guide/android/com/androidguide/activities/MasterActivity.java
@@ -0,0 +1,23 @@
+package guide.android.com.androidguide.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import guide.android.com.androidguide.R;
+
+public class MasterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master);
+
+
+
+    }
+
+}
